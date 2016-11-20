8844f903efd35cb5cc9a9c165e4c6fff7f6e8f95
ben <ben.spoor@gmail.com>
2016-11-20 22:00:42 +0100

    Added range library

---
+++ src/range/range.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include "range.h"
+
+/*! \brief Initializes the range library
+
+The range library is initialized and ready to use.
+*/
+int range_init()
+{
+    return (int) RANGE_OK;
+}
+
