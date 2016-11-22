7a8c487220c6d169fa2125f05b649e151a647bd9
ben <ben.spoor@gmail.com>
2016-11-22 21:55:58 +0100

    Add schedule and get_result

--- src/range/range.h
+++ src/range/range.h
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/*! Temporary public global for simulating sensor reading */
+unsigned char range_scan_result = 0x00;
+
 /*! error code for library */
-typedef enum {RANGE_OK = 0,         /*!< Everything went OK */ \
-              RANGE_PARAMETER_ERROR /*!< Wrong parameter was provided */
+typedef enum {RANGE_OK = 0,          /*!< Everything went OK */ \
+              RANGE_PARAMETER_ERROR, /*!< Wrong parameter was provided */
+              RANGE_NO_REQUEST_ERROR /*!< Scan not requested before getting result */
              } RangeError;
 
 extern int range_init();
+
+extern int range_schedule_scan();
+
+extern int range_get_result();
--- tst/test_librange.c
+++ tst/test_librange.c
@@ -17,11 +17,77 @@ START_TEST (test_init)
 }
 END_TEST
 
+/*! \brief Test scheduling & retrieving result
+*/
+START_TEST (test_schedule_and_retrieve_GW001)
+{
+    unsigned char actual_result  = 0;
+
+    /* Setup */
+    CALL( range_init());
+    range_scan_result = 42;
+
+    /* Execute */
+    CALL(range_schedule_scan());
+    CALL(range_get_result(&actual_result));
+
+    /* Verify */
+    ck_assert_int_eq(actual_result, range_scan_result);
+
+    /* Teardown */
+}
+END_TEST
+
+
+/*! \brief Test retrieving result without schedule
+*/
+START_TEST (test_schedule_and_retrieve_BW001)
+{
+    int result = (int) RANGE_OK;
+    unsigned char actual_result  = 0;
+
+    /* Setup */
+    CALL( range_init());
+
+    /* Execute */
+    result = range_get_result(&actual_result);
+
+    /* Verify */
+    ck_assert(result == (int) RANGE_NO_REQUEST_ERROR);
+
+    /* Teardown */
+}
+END_TEST
+
+
+/*! \brief Test retrieving result without schedule
+*/
+START_TEST (test_schedule_and_retrieve_BW002)
+{
+    int result = (int) RANGE_OK;
+
+    /* Setup */
+    CALL( range_init());
+
+    /* Execute */
+    result = range_get_result(NULL);
+
+    /* Verify */
+    ck_assert(result == (int) RANGE_PARAMETER_ERROR);
+
+    /* Teardown */
+}
+END_TEST
+
 Suite* motion (void) {
         Suite *suite = suite_create("range");
         TCase *tcase = tcase_create("GW");
 
         tcase_add_test(tcase, test_init);
+
+        tcase_add_test(tcase, test_schedule_and_retrieve_GW001);
+        tcase_add_test(tcase, test_schedule_and_retrieve_BW001);
+        tcase_add_test(tcase, test_schedule_and_retrieve_BW002);
 
         suite_add_tcase(suite, tcase);
         return suite;
