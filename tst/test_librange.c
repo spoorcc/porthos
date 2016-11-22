4eed42ff93b946bb331bc324d4c709f8362523b0
ben <ben.spoor@gmail.com>
2016-11-22 22:14:23 +0100

    Fixed some typos

--- tst/test_librange.c
+++ tst/test_librange.c
@@ -9,7 +9,7 @@
 
 /*! \brief Test initialization
 
-Test initialization of Motion library
+Test initialization of Range library
 */
 START_TEST (test_init)
 {
@@ -79,7 +79,7 @@ START_TEST (test_schedule_and_retrieve_BW002)
 }
 END_TEST
 
-Suite* motion (void) {
+Suite* range (void) {
         Suite *suite = suite_create("range");
         TCase *tcase = tcase_create("GW");
 
@@ -95,7 +95,7 @@ Suite* motion (void) {
 
 int main (int argc, char *argv[]) {
         int number_failed;
-        Suite *suite = motion();
+        Suite *suite = range();
         SRunner *runner = srunner_create(suite);
 
         srunner_run_all(runner, CK_NORMAL);
