092ae024d6110fabfb0b130ffac41f252cefd472
ben <ben.spoor@gmail.com>
2017-01-21 23:11:05 +0100

    Added dir and files

---
+++ lib/src/simulator/simulator.c
@@ -0,0 +1,11 @@
+#include "porthos/simulator.h"
+
+
+/*! \brief Initializes the simulator
+
+The simulator library is initialized and ready to use.
+*/
+int simulator_init()
+{
+    return (int) SIMULATOR_OK;
+}
